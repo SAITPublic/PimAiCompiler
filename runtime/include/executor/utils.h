f990a38b26f7f1d9a4a613f4d28f0d9df8dda73c
jiwei-yu <jiwei.yu@samsung.com>
2021-07-13 04:03:57 +0800

    feat: 🎸 runtime utils for DataType and MemoryFormat convert
    runtime utils for DataType and MemoryFormat convert.
    
    Signed-off-by: jiwei-yu <jiwei.yu@samsung.com>

--- runtime/include/executor/utils.h
+++ runtime/include/executor/utils.h
@@ -2,6 +2,10 @@
 
 #include <torch/script.h>
 
+#include "ir/include/nn_ir.hpp"
+
+namespace nncir = nn_compiler::nn_ir;
+
 namespace nnrt
 {
 template <typename T>
@@ -15,5 +19,9 @@ torch::jit::IValue tensorToIValue(const torch::Tensor& tensor);
 torch::jit::IValue tensorListToIValue(const torch::TensorList& tensor_list);
 
 torch::jit::IValue strToIValue(std::string str);
+
+at::ScalarType convertDTypeToATScalarType(nncir::DataType dtype);
+
+at::MemoryFormat getMemoryFormat(int optional_memory_format);
 
 }  // namespace nnrt
--- runtime/source/executor/utils.cpp
+++ runtime/source/executor/utils.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <torch/script.h>
 
+#include "executor/utils.h"
+
 namespace nnrt
 {
 
@@ -9,5 +11,47 @@ torch::jit::IValue tensorToIValue(const torch::Tensor& tensor) { return torch::j
 torch::jit::IValue tensorListToIValue(const torch::TensorList& tensor_list) { return torch::jit::IValue(tensor_list); }
 
 torch::jit::IValue strToIValue(std::string str) { return torch::jit::IValue(str); }
+
+at::ScalarType convertDTypeToATScalarType(nncir::DataType dtype) {
+    // according to: pytorch/c10/core/ScalarType.h
+    switch (dtype) {
+        case nncir::DataType::UINT8 :
+            return at::ScalarType::Byte;
+        case nncir::DataType::INT8 :
+            return at::ScalarType::Char;
+        case nncir::DataType::INT16 :
+            return at::ScalarType::Short;
+        case nncir::DataType::INT32 :
+            return at::ScalarType::Int;
+        case nncir::DataType::INT64 :
+            return at::ScalarType::Long;
+        case nncir::DataType::FLOAT16 :
+            return at::ScalarType::Half;
+        case nncir::DataType::FLOAT32 :
+            return at::ScalarType::Float;
+        case nncir::DataType::FLOAT64 :
+            return at::ScalarType::Double;
+        case nncir::DataType::BOOL :
+            return at::ScalarType::Bool;
+        default:
+            DLOG(ERROR) << "Complex type has not been supported.";
+    }
+}
+
+at::MemoryFormat getMemoryFormat(int optional_memory_format) {
+    // according to: pytorch/c10/core/MemoryFormat.h
+    switch (optional_memory_format) {
+        case 0 :
+            return at::MemoryFormat::Contiguous;
+        case 1 :
+            return at::MemoryFormat::Preserve;
+        case 2 :
+            return at::MemoryFormat::ChannelsLast;
+        case 3 :
+            return at::MemoryFormat::ChannelsLast3d;
+        default :
+            DLOG(ERROR) << "Wrong reference to aten MemoryFormat.";
+    }
+}
 
 }  // namespace nnrt
