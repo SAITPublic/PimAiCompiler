b1fc899e4d72ccd1645cc180cf5dbea946299b5e
jie01.wu <jie01.wu@samsung.com>
2021-07-15 03:32:03 +0800

    feat: 🎸 support determine the default value
    Add an interface to determine the default value
    
    Signed-off-by: jie01.wu <jie01.wu@samsung.com>

---
+++ ir/include/common/utils.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "ir/include/common/log.hpp"
+
+#include <typeinfo>
+
+#include <float.h>
+
+#include <math.h>
+
+namespace nn_compiler::nn_ir {
+
+template <typename T>
+bool isDefaultValue(T& input) {
+    int a;
+    int64_t b;
+    float c;
+    double d;
+    if (typeid(input).name() == typeid(a).name() && input == INT32_MAX) {
+        return true;
+    } else if (typeid(input).name() == typeid(b).name() && input == INT64_MAX) {
+        return true;
+    } else if (typeid(input).name() == typeid(c).name() && (fabs(input - FLT_MAX) <= FLT_EPSINON)) {
+        return true;
+    } else if (typeid(input).name() == typeid(d).name() && (fabs(input - DBL_MAX) <= DBL_EPSINON)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+}  // namespace nn_compiler::nn_ir
