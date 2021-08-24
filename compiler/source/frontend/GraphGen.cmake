216ca5bc4e0155adc090c150e9df1c8af9647788
Guoqiang He <guoq.he@samsung.com>
2021-08-24 18:45:31 +0000

    fix: 🐛 Fix the mismatch issue of cmake version
    Because target_link_directories was added in CMake 3.13, but default
    Cmake version in docker is 3.10. It will cause build error in Jenkins
    CI. More imported, target_link_directories is not recommended in CMake
    official manual. More detail can be checked at links below:
    https://cmake.org/cmake/help/latest/command/target_link_directories.html
    
    Signed-off-by: Guoqiang He <guoq.he@samsung.com>

--- compiler/source/frontend/GraphGen.cmake
+++ compiler/source/frontend/GraphGen.cmake
@@ -7,21 +7,12 @@ target_include_directories(frontend PUBLIC
         ${GRAPHGEN_DIR}/core/misc
         ${GRAPHGEN_DIR}/third_party/nlohmann)
 
-target_link_directories(frontend PUBLIC 
-        ${GRAPHGEN_BUILD_DIR}/core
-        ${GRAPHGEN_BUILD_DIR}/core/export
-        ${GRAPHGEN_BUILD_DIR}/core/graphgen_network
-        ${GRAPHGEN_BUILD_DIR}/core/import
-        ${GRAPHGEN_BUILD_DIR}/core/intermediate_process
-        ${GRAPHGEN_BUILD_DIR}/core/misc
-        ${GRAPHGEN_BUILD_DIR}/third_party/glog/glog-build)
-
-target_link_libraries(frontend graphgen_core
-        graphgen_export
-        graphgen_network
-        graphgen_import
-        graphgen_intermediate_process
-        ir_builder
-        plugin_chain
-        graphgen_misc
-        glog)
+foreach(libname graphgen_core graphgen_export graphgen_network
+        graphgen_import graphgen_intermediate_process ir_builder plugin_chain graphgen_misc glog)
+        find_library(lib${libname}
+                    NAMES ${libname}
+                    HINTS ${GRAPHGEN_BUILD_DIR}
+                    PATH_SUFFIXES core core/export core/graphgen_network core/import
+                                  core/intermediate_process core/misc third_party/glog/glog-build)
+        target_link_libraries(frontend ${lib${libname}})
+endforeach()
