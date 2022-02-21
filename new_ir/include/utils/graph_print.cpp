3907a10310f11522bd632560614fe0744c410a5c
yao01.xiao <yao01.xiao@samsung.com>
2022-02-21 10:08:39 -0500

    refactor: 💡 Add graph print in utils folder for refactoring
    Add graph print in utils folder for refactoring
    
    Signed-off-by: yao01.xiao <yao01.xiao@samsung.com>

---
+++ new_ir/include/utils/graph_print.cpp
@@ -0,0 +1,77 @@
+#include "new_ir/include/utils/graph_print.h"
+
+#include "new_ir/include/layers/prim_if_layer.h"
+#include "new_ir/include/layers/prim_loop_layer.h"
+
+namespace nn_compiler {
+namespace ir {
+
+void printGraphModel(std::unique_ptr<ir::NNModel>& nn_model) {
+    std::string tab = "    ";
+    auto graphs = nn_model->getGraphs();
+    for (auto graph : graphs) {
+        auto gin_ids = graph->getGraphInTensorID();
+        std::string network_line;
+        network_line.clear();
+        network_line.append(graph->getName() + " (");
+        for (auto iid : gin_ids) {
+            network_line = network_line.append(
+                std::to_string(iid) + " : " +
+                nn_model->getTSSTensors()[iid]->getReprType() + ", ");
+        }
+        network_line.append(") :");
+        DLOG(INFO) << network_line;
+
+        std::string layer_line;
+        for (auto layer : graph->getLayers()) {
+            layer_line.clear();
+            std::string attr = "";
+            if (layer->getType() == "prim::If") {
+                attr =
+                    "[ThenNet = " +
+                    std::dynamic_pointer_cast<nn_compiler::ir::PrimIfLayer>(
+                        layer)
+                        ->getThenNet() +
+                    ", ElseNet = " +
+                    std::dynamic_pointer_cast<nn_compiler::ir::PrimIfLayer>(
+                        layer)
+                        ->getElseNet() +
+                    "]";
+            }
+            if (layer->getType() == "prim::Loop") {
+                attr = "[BodyNet = " +
+                    std::dynamic_pointer_cast<
+                        nn_compiler::ir::PrimLoopLayer>(layer)
+                        ->getBodyNet() +
+                    "]";
+            }
+            layer_line.append(tab);
+            auto in_ids = layer->getInSTensorID();
+            auto out_ids = layer->getOutSTensorID();
+            for (auto oid : out_ids) {
+                layer_line.append(
+                    std::to_string(oid) + " : " +
+                    nn_model->getTSSTensors()[oid]->getReprType() + ", ");
+            }
+            layer_line.append(" = " + layer->getType() + attr + " (");
+            for (auto iid : in_ids) {
+                layer_line.append(std::to_string(iid) + ", ");
+            }
+            layer_line.append(")");
+            DLOG(INFO) << layer_line;
+        }
+
+        auto gout_ids = graph->getGraphOutTensorID();
+        std::string return_line;
+        return_line.clear();
+        return_line.append(tab + "return (");
+        for (auto oid : gout_ids) {
+            return_line.append(std::to_string(oid) +  ", ");
+        }
+        return_line.append(") \n\n");
+        DLOG(INFO) << return_line;
+    }
+}
+
+} // namespace ir
+} // namespace nn_compiler
---
+++ new_ir/include/utils/graph_print.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "new_ir/include/layers/nn_layer.h"
+#include "new_ir/include/nn_model.h"
+
+namespace nn_compiler {
+namespace ir {
+
+void printGraphModel(std::unique_ptr<ir::NNModel>& nn_model);
+
+} // namespace ir
+} // namespace nn_compiler
